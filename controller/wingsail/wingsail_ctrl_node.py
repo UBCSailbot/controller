91e5cf5beba3eb9acd083331444df6072a51f96a
Alberto <albertoescobar@live.com>
2023-11-25 17:03:40 -0800

    cleaned up subscriptio logic

--- controller/wingsail/wingsail_ctrl_node.py
+++ controller/wingsail/wingsail_ctrl_node.py
@@ -23,7 +23,8 @@ class WingsailControllerNode(Node):
     while optimizing for speed by maximizing the lift-to-drag ratio of the wingsail.
 
     Subscriptions:
-        TO BE ADDED
+        __filtered_wind_sensors_sub (Subscription): Subscribes to a `WindSensor` message.
+        __gps_sub (Subscription): Subscribes to a 'GPS' message.
 
     Publishers:
         TO BE ADDED
@@ -45,6 +46,8 @@ class WingsailControllerNode(Node):
         during the initialization process.
         """
         self.__trim_tab_angle = 0.0
+        self.filtered_wind_sensor = None
+        self.gps = None
 
     def __declare_ros_parameters(self):
         """Declares ROS parameters from the global configuration file that will be used in this
@@ -75,16 +78,16 @@ class WingsailControllerNode(Node):
         # Callbacks for each subscriptions should be defined as private methods of this class
         self.get_logger().debug("Initializing subscriptions...")
 
-        self.__filtered_wind_sensors_sub = self.create_subscription(
+        self.__filtered_wind_sensor_sub = self.create_subscription(
             msg_type=WindSensor,
             topic="filtered_wind_sensor",
-            callback=self.__wind_sensors_sub_callback,
+            callback=self.__filtered_wind_sensor_sub_callback,
             qos_profile=1,
         )
 
         self.__gps_sub = self.create_subscription(
             msg_type=GPS,
-            topic="mock_gps",
+            topic="gps",
             callback=self.__gps_sub_callback,
             qos_profile=1,
         )
@@ -106,25 +109,13 @@ class WingsailControllerNode(Node):
     def trim_tab_angle(self) -> float:
         return self.__trim_tab_angle
 
-    # @property
-    # def filtered_wind_sensors_sub(self) -> Subscription:
-    #     return self.__filtered_wind_sensors_sub
-
-    # @property
-    # def gps_sub(self) -> Subscription:
-    #     return self.__gps_sub
-
-    def __wind_sensors_sub_callback(self, msg):
-        self.filtered_wind_sensors = msg
-        self.get_logger().info(f"Received data from {self.__filtered_wind_sensors_sub.topic}")
-        self.get_logger().info(f"Man IDK the format of this: {self.filtered_wind_sensors}")
+    def __filtered_wind_sensor_sub_callback(self, msg):
+        self.filtered_wind_sensor = msg
+        self.get_logger().info(f"Received data from {self.__filtered_wind_sensor_sub.topic}")
 
     def __gps_sub_callback(self, msg):
         self.gps = msg
         self.get_logger().info(f"Received data from {self.__gps_sub.topic}")
-        self.get_logger().info(
-            f"Also do not know the format of this: {self.filtered_wind_sensors}"
-        )
 
 
 if __name__ == "__main__":
