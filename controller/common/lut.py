e2de4fd5d7ad7526db37acf639e09829ec3236ef
Alberto <albertoescobar@live.com>
2024-01-20 18:09:12 -0800

    implemented initial implementation of linear and spline interpolation

--- controller/common/lut.py
+++ controller/common/lut.py
@@ -1,6 +1,9 @@
+import numpy as np
+
+
 class LUT:
     def __init__(self, lookup_table, interpolation_method="linear"):
-        self.table = lookup_table
+        self.table = np.array(lookup_table)
         self.interpolation = interpolation_method
 
     def __call__(self, x):
@@ -12,7 +15,8 @@ class LUT:
             raise ValueError(self.interpolation + " is an unknown interpolation method!")
 
     def linearInterpolation(self, x):
-        return 0
+        return np.interp(x, self.table[:, 0], self.table[:, 1])
 
     def splineInterpolation(self, x):
-        return 0
+        cs = np.interpolate.CubicSpline(self.table[:, 0], self.table[:, 1])
+        return cs(x)
