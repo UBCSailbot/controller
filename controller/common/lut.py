078a42818cc1cb4000a12d6391355c85736ec7b4
Alberto <albertoescobar@live.com>
2024-01-20 23:22:48 -0800

    added basic test to test_lut and added robustness to constructor of lut

--- controller/common/lut.py
+++ controller/common/lut.py
@@ -5,6 +5,8 @@ class LUT:
     def __init__(self, lookup_table, interpolation_method="linear"):
         self.table = np.array(lookup_table)
         self.interpolation = interpolation_method
+        if self.interpolation != "linear" and self.interpolation != "spline":
+            raise ValueError(self.interpolation + " is an unknown interpolation method!")
 
     def __call__(self, x):
         if self.interpolation == "linear":
@@ -12,7 +14,7 @@ class LUT:
         elif self.interpolation == "spline":
             return self.splineInterpolation(x)
         else:
-            raise ValueError(self.interpolation + " is an unknown interpolation method!")
+            return 0
 
     def linearInterpolation(self, x):
         return np.interp(x, self.table[:, 0], self.table[:, 1])
--- tests/unit/wingsail/common/test_lut.py
+++ tests/unit/wingsail/common/test_lut.py
@@ -1,1 +1,16 @@
 # import pytest
+
+from controller.common.lut import LUT
+
+
+class TestLUT:
+    def test_LUT_constructor(self):
+        look_up_table = [[50000, 5.75], [100000, 6.75], [200000, 7], [500000, 9.75], [1000000, 10]]
+        testLUT = LUT(look_up_table)
+        assert testLUT(40000) == 5.75
+
+    # def test_linear_interpolation
+
+    # def test_spline_interpolation
+
+    # test_LUT_constructor
