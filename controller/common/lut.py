e09a58d8ea5523367f70d93416c54ba653f48708
Alberto <albertoescobar@live.com>
2024-01-27 14:55:32 -0800

    documented LUT class

--- controller/common/lut.py
+++ controller/common/lut.py
@@ -3,7 +3,26 @@ from scipy import interpolate
 
 
 class LUT:
+    """
+    Class for performing look-up table interpolation.
+
+    Methods:
+        __init__: Initializes the LUT object with lookup table data and interpolation method.
+        __call__: Calls the interpolation method with the given input.
+    """
+
     def __init__(self, lookup_table: list, interpolation_method: str = "linear"):
+        """
+        Initializes the LUT object.
+
+        Args:
+            lookup_table (list): A list of tuples or lists containing x-y data points for
+            interpolation. Format is [[x-values],[y-values]]
+            interpolation_method (str): Interpolation method to use. Default is "linear".
+
+        Raises:
+            ValueError: If the specified interpolation method is unknown.
+        """
         self.__table = np.array(lookup_table)
         self.__interpolation = interpolation_method
 
@@ -16,6 +35,16 @@ class LUT:
                 raise ValueError(self.__interpolation + " is an unknown interpolation method!")
 
     def __call__(self, x: float) -> float:
+        """
+        Calls the interpolation method with the given input.
+
+        Args:
+            x (float): The input value to interpolate.
+
+        Returns:
+            float: The interpolated value using the interpolation method defined when LUT instance
+            creation.
+        """
         return self.__method(x)
 
     def __linearInterpolation(self, x: float) -> float:
