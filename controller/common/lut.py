34968983dfdc4b56e486abba1b20a2c5f9738ec0
Alberto <albertoescobar@live.com>
2024-02-03 17:48:30 -0800

    resolved issues raised in PR#25

--- controller/common/lut.py
+++ controller/common/lut.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 import numpy as np
 from scipy import interpolate
 
@@ -11,19 +13,31 @@ class LUT:
         __call__: Calls the interpolation method with the given input.
     """
 
-    def __init__(self, lookup_table: list, interpolation_method: str = "linear"):
+    def __init__(
+        self,
+        lookup_table: Union[List[List[int | float]], np.ndarray],
+        interpolation_method: str = "linear",
+    ):
         """
         Initializes the LUT object.
 
         Args:
             lookup_table (list): A list of tuples or lists containing x-y data points for
-            interpolation. Format is [[x-values],[y-values]]
+            interpolation. Format is [[x1,y1], [x2,y2], ..., [xn,yn]]
             interpolation_method (str): Interpolation method to use. Default is "linear".
 
         Raises:
             ValueError: If the specified interpolation method is unknown.
         """
-        self.__table = np.array(lookup_table)
+        if type(lookup_table) is np.ndarray:
+            self.__table = lookup_table
+        else:
+            self.__table = np.array(lookup_table)
+
+        self.verifyTable(self.__table)
+        self.x = self.__table[:, 0]
+        self.y = self.__table[:, 1]
+
         self.__interpolation = interpolation_method
 
         match self.__interpolation:
@@ -48,8 +62,17 @@ class LUT:
         return self.__method(x)
 
     def __linearInterpolation(self, x: float) -> float:
-        return np.interp(x, self.__table[:, 0], self.__table[:, 1])
+        return np.interp(x, self.x, self.y)
 
     def __splineInterpolation(self, x: float) -> float:
-        cs = interpolate.CubicSpline(self.__table[:, 0], self.__table[:, 1])
+        cs = interpolate.CubicSpline(self.x, self.y)
         return cs(x)
+
+    def verifyTable(self, table: np.ndarray) -> None:
+        if len(table.shape) == 2:
+            if table.shape[1] == 2:
+                return
+            else:
+                raise ValueError("Input table is invalid shape.")
+        else:
+            raise ValueError("Input table is invalid shape.")
--- tests/unit/wingsail/common/test_lut.py
+++ tests/unit/wingsail/common/test_lut.py
@@ -1,6 +1,7 @@
-# import pytest
+import math
 
 import numpy as np
+import pytest
 from scipy import interpolate
 
 from controller.common.lut import LUT
@@ -15,39 +16,48 @@ class TestLUT:
         testLUT = LUT(self.look_up_table)
 
         # test that LUT return a known value
-        assert testLUT(50000) == 5.75
+        assert math.isclose(testLUT(50000), 5.75)
 
     def test_unknown_interpolation_exception(self):
-        try:
+        with pytest.raises(ValueError):
             testLUT = LUT(self.look_up_table, "gabagool")
-            assert False  # failure: constructor accepted deli meat interpolation method
 
-        except ValueError:
-            assert True
+    def test_invalid_table_exception(self):
+        invalid_table1 = [[10000, 10000, 10000], [1, 1, 1]]
+        invalid_table2 = [10000, 10000, 10000]
+        invalid_table3 = [[0, 1], 10000, 10000]
 
-        except:
-            assert False  # failure: constructor threw wrong exception
+        with pytest.raises(ValueError):
+            testLUT = LUT(invalid_table1)
 
-    def test_linear_interpolation(self):
+        with pytest.raises(ValueError):
+            testLUT = LUT(invalid_table2)
+
+        with pytest.raises(ValueError):
+            testLUT = LUT(invalid_table3)
+
+    @pytest.mark.parametrize("linear_test_values", list(range(50000, 1100000, 10000)))
+    def test_linear_interpolation(self, linear_test_values):
         # set up
         testLUT = LUT(self.look_up_table)
         table = np.array(self.look_up_table)
-        test_values = list(range(50000, 1100000, 10000))
-
-        # Test that linear interpolation does not extrapolate
-        assert testLUT(10000) == 5.75
-        assert testLUT(2000000) == 10
 
         # Test that LUT returns same values as np linear interpolate function
-        for value in test_values:
-            assert testLUT(value) == np.interp(value, table[:, 0], table[:, 1])
+        assert math.isclose(
+            testLUT(linear_test_values), np.interp(linear_test_values, table[:, 0], table[:, 1])
+        )
 
-    def test_spline_interpolation(self):
+    def test_linear_extrapolation(self):
+        testLUT = LUT(self.look_up_table)
+        # Test that linear interpolation does not extrapolate
+        assert math.isclose(testLUT(10000), 5.75)
+        assert math.isclose(testLUT(2000000), 10)
+
+    @pytest.mark.parametrize("spline_test_values", list(range(10000, 2100000, 10000)))
+    def test_spline_interpolation_extrapolation(self, spline_test_values):
         testLUT = LUT(self.look_up_table, "spline")
         table = np.array(self.look_up_table)
-        test_values = list(range(10000, 2100000, 10000))
         cs = interpolate.CubicSpline(table[:, 0], table[:, 1])
 
         # Test that LUT returns same values as cubic interpolate function
-        for value in test_values:
-            assert testLUT(value) == cs(value)
+        assert math.isclose(testLUT(spline_test_values), cs(spline_test_values))
