a2164101ebac8361264169f2ab29cbbdbd7aad03
Evan Nawfal <evanxnawfal@gmail.com>
2024-02-17 15:14:48 -0800

    Wingsail controller with tests

--- controller/common/constants.py
+++ controller/common/constants.py
@@ -1,1 +1,4 @@
 """Constants used across the controller package."""
+
+CHORD_WIDTH_MAIN_SAIL = 0.14  # meters, trim tab chord width is not included
+KINEMATIC_VISCOSITY = 0.000014207  # {m^2 / s at 10degC} and air density at 1.225 {kg / m^3}
